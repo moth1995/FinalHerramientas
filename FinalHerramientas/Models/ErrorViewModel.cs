3d3f0e9a8489e3fed25164e32f2336a9efe01e69
moth1995 <marcos-flores@live.com.ar>
2024-07-04 19:16:57 -0300

    Add project files.

---
+++ FinalHerramientas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FinalHerramientas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
